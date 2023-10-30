483fd0b87ff58b23869abf40b79765940e8b08af
AndryCun <ulhad321@mhs.mdp.ac.id>
2023-10-30 10:24:29 +0700

    update lagi

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: HomeScreen(),
       theme: ThemeData(
-        primaryColor: Colors.blue, // Atur warna menjadi biru
+        primaryColor: Colors.blue,
       ),
     );
   }
@@ -21,27 +21,25 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("City Tour"), // Atur title appbar
+        title: Text("City Tour"),
       ),
       body: Padding(
-        padding: EdgeInsets.all(16.0), // Bungkus dengan padding
+        padding: EdgeInsets.all(16.0),
         child: SingleChildScrollView(
           child: Column(
             children: [
-              SizedBox(height: 16.0), // Jarak antar-widget
-              // Galeri Gambar
+              SizedBox(height: 16.0),
               SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: Row(
                   children: [
-                    buildImage("https://picsum.photos/id/66/300/200"),
-                    buildImage("https://picsum.photos/id/76/300/200"),
-                    buildImage("https://picsum.photos/id/86/300/200"),
+                    ImageCard("https://picsum.photos/id/66/300/200"),
+                    ImageCard("https://picsum.photos/id/76/300/200"),
+                    ImageCard("https://picsum.photos/id/86/300/200"),
                   ],
                 ),
               ),
-              SizedBox(height: 16.0), // Jarak antar-widget
-              // Tagline
+              SizedBox(height: 16.0),
               Text(
                 "Explore, Enjoy, Experience",
                 style: TextStyle(
@@ -49,30 +47,69 @@ class HomeScreen extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              SizedBox(height: 16.0), // Jarak antar-widget
-              // Deskripsi
-              Text(
-                "Explore the charm of our city's hidden gems and embark on a journey to discover the rich cultural heritage, breathtaking landscapes, and unique experiences that await you. From historic landmarks to vibrant markets and serene natural wonders, our city offers a tapestry of experiences that will captivate your senses. Join us in exploring the beauty of our city and create memories that will last a lifetime. Come and visit our city's treasures today.",
-                textAlign: TextAlign.justify,
-              ),
-              SizedBox(height: 16.0), // Jarak antar-widget
-              // Gambar "Blue body of water"
-              Image.network(
-                "https://picsum.photos/id/66/200/300",
-                width: double.infinity, // Lebar gambar selebar layar
-                height: 250.0, // Tinggi gambar 250 pixel
-                fit: BoxFit.cover,
-              ),
-              Text(
-                "Blue body of water",
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+              SizedBox(height: 16.0),
+              DescriptionText(),
+              SizedBox(height: 16.0),
+              ImageWithCaption(),
             ],
           ),
         ),
       ),
     );
   }
+}
+
+class ImageCard extends StatelessWidget {
+  final String imageUrl;
+
+  ImageCard(this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(right: 16.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8.0),
+        child: Image.network(
+          imageUrl,
+          width: 150.0,
+          height: 300.0,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class DescriptionText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "Explore the charm of our city's hidden gems and embark on a journey to discover the rich cultural heritage, breathtaking landscapes, and unique experiences that await you. From historic landmarks to vibrant markets and serene natural wonders, our city offers a tapestry of experiences that will captivate your senses. Join us in exploring the beauty of our city and create memories that will last a lifetime. Come and visit our city's treasures today.",
+      textAlign: TextAlign.justify,
+    );
+  }
+}
+
+class ImageWithCaption extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image.network(
+          "https://picsum.photos/id/66/200/300",
+          width: double.infinity,
+          height: 250.0,
+          fit: BoxFit.cover,
+        ),
+        Text(
+          "Blue body of water",
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
+  }
+}
